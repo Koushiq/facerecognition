91a52a5b4375a85767714723725e07c1ca49eec4
Koushiq <koushiq1234@gmail.com>
2020-09-02 07:02:55 +0100

    Folder Structure corrected

--- findfaces.py
+++
@@ -1,9 +0,0 @@
-import face_recognition
-
-image = face_recognition.load_image_file('./img/groups/koushiq.jpg')
-
-face_locations = face_recognition.face_locations(image)
-
-print(face_locations)
-
-print(f'There are {len(face_locations)} people in this image')
