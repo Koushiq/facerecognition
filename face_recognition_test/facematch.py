8bae00c22ff600a997cd3d92423e0f562297f450
Koushiq <koushiq1234@gmail.com>
2020-09-02 06:58:54 +0100

    test modules for opecv added , direcotory structure changed

---
+++ face_recognition_test/facematch.py
@@ -0,0 +1,19 @@
+import face_recognition
+
+image_of_koushiq = face_recognition.load_image_file('./img/known/koushiq.jpg')
+
+koushiq_face_encoding = face_recognition.face_encodings(image_of_koushiq)[0]
+
+unknown_image = face_recognition.load_image_file('./img/unknown/unkoushiq.jpg')
+
+unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
+
+
+results = face_recognition.compare_faces([koushiq_face_encoding] , unknown_face_encoding )
+
+if results[0]:
+    print('Face of koushiq')
+else:
+    print('not face of koushiq')
+
+
