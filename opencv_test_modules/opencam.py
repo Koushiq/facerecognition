4cec1bc90ee8323412fa0a805b9d4ae0ee7ebad8
koushiq <koushiq1234@gmail.com>
2020-09-05 12:02:25 +0100

    modularity made, can be imported and used

--- opencv_test_modules/opencam.py
+++ opencv_test_modules/opencam.py
@@ -1,21 +1,22 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+def triggerCam():
+    cap = cv2.VideoCapture(0)
+    ret2, frame2 = cap.read()
+    cv2.imshow('frame2',frame2)
+    while(True):
+        
+        ret, frame = cap.read()
+        
+        cv2.imshow('frame',frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-#cap.set()
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    
-    # Our operations on the frame come here
-    
-    # Display the resulting frame
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cap.release()
+    cv2.destroyAllWindows()
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+
+triggerCam()
--- opencv_test_modules/recogniser.py
+++ opencv_test_modules/recogniser.py
@@ -2,80 +2,89 @@ import face_recognition
 import cv2
 import numpy as np
 import get_files as files
+import time
 
-video_capture = cv2.VideoCapture(0)
-knownFaceEncodings=[]
-knownFaceNames=[]
+def triggerFaceDetection():
+    video_capture = cv2.VideoCapture(0)
+    knownFaceEncodings=[]
+    knownFaceNames=[]
 
-cameraFaceLocations=[]
-cameraFaceEncodings=[]
-faceNames=[]
+    cameraFaceLocations=[]
+    cameraFaceEncodings=[]
+    faceNames=[]
 
-processFrame = True
+    processFrame = True
 
-images = files.get_dir_files('imgs')
+    images = files.get_dir_files('imgs')
 
-print (images)
+    print (images)
 
-knownFaceNames=files.get_file_name_all('imgs')
-print (knownFaceNames)
-
-
-for path in images:
-    knownImage = face_recognition.load_image_file("imgs/"+path)
-    knownFaceEncodings.append(face_recognition.face_encodings(knownImage)[0])
+    knownFaceNames=files.get_file_name_all('imgs')
+    print (knownFaceNames)
 
 
-while True:
-    ret, frame = video_capture.read()
+    for path in images:
+        knownImage = face_recognition.load_image_file("imgs/"+path)
+        knownFaceEncodings.append(face_recognition.face_encodings(knownImage)[0])
 
-    smallFrame =cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    rgbSmallFrame= smallFrame[:, :, ::-1]
+    while True:
+        ret, frame = video_capture.read()
 
-    if processFrame:
-        cameraFaceLocations = face_recognition.face_locations(rgbSmallFrame)
-        cameraFaceEncodings = face_recognition.face_encodings(rgbSmallFrame,cameraFaceLocations)
-        faceNames=[]
+        smallFrame =cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        for cameraFaceEncoding in cameraFaceEncodings:
-            #make this multiprocess
-            matches = face_recognition.compare_faces(knownFaceEncodings,cameraFaceEncoding)            
-            name="Unrecognized"
+        rgbSmallFrame= smallFrame[:, :, ::-1]
+
+        if processFrame:
+            cameraFaceLocations = face_recognition.face_locations(rgbSmallFrame)
+            cameraFaceEncodings = face_recognition.face_encodings(rgbSmallFrame,cameraFaceLocations)
+            faceNames=[]
+
+            for cameraFaceEncoding in cameraFaceEncodings:
+                #make this multiprocess
+                matches = face_recognition.compare_faces(knownFaceEncodings,cameraFaceEncoding)            
+                name="Unrecognized"
+                
+                faceDistances = face_recognition.face_distance(knownFaceEncodings,cameraFaceEncoding)
+                bestMatchIndex= np.argmin(faceDistances)
+
+                if matches[bestMatchIndex]:
+                    name=knownFaceNames[bestMatchIndex]
+
+                faceNames.append(name)
+
+        processFrame=not processFrame
+
+
+        for(top,right,bottom,left), name in zip(cameraFaceLocations,faceNames):
+
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 1)
+
+
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
             
-            faceDistances = face_recognition.face_distance(knownFaceEncodings,cameraFaceEncoding)
-            bestMatchIndex= np.argmin(faceDistances)
-
-            if matches[bestMatchIndex]:
-                name=knownFaceNames[bestMatchIndex]
-
-            faceNames.append(name)
-
-    processFrame=not processFrame
+            print("name is :" + name)
+            
+            
+        cv2.imshow('Video',frame)
 
 
-    for(top,right,bottom,left), name in zip(cameraFaceLocations,faceNames):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 1)
-
-
-        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
-
-    cv2.imshow('Video',frame)
+    video_capture.release()
+    cv2.destroyAllWindows()
+    
+triggerFaceDetection()
 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-
-video_capture.release()
-cv2.destroyAllWindows()
+    
 
     
